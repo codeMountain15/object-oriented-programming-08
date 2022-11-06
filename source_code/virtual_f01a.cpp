a0c8751cdfdd5283379676cfd3cd863ea6a284b1
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-06 14:12:00 +0200

    Create virtual_f01a.cpp

---
+++ source_code/virtual_f01a.cpp
@@ -0,0 +1,40 @@
+// virtual_f01a.cpp
+// extending base class function
+
+#include<iostream>
+using namespace std;
+
+class hyperclass {
+public:
+    virtual void hello() { cout << "hello() from hyperclass\n"; }
+
+    void yolo() { cout << "yolo() from hyperclass\n"; }
+};
+
+class subclass : public hyperclass {
+public:
+    // is overridden, so it is also considered as virtual
+    // without declaring it
+    void hello() { cout << "hello() from subclass\n"; }
+
+    void yolo() { // extending hyperclass yolo
+        cout << "yolo() from subclass and ...\n"; 
+        hyperclass::yolo();
+    }
+};
+
+int main() {
+    hyperclass* hpr;
+    subclass sb;
+    hpr = &sb;
+
+    // Virtual function
+    // -> binded at runtime
+    hpr->hello();
+
+    // Non-virtual function
+    // -> binded at compile time
+    hpr->yolo();
+
+    return 0;
+}
