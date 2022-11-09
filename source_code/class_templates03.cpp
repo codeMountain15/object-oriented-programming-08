a422236a501e89a69d33ae63f6dc22e3caafb35a
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-09 23:55:05 +0200

    Update class_templates03.cpp

--- source_code/class_templates03.cpp
+++ source_code/class_templates03.cpp
@@ -1,18 +1,20 @@
-// class_templates03.cpp
+// templates03.cpp
 // with pre-defined value
 
 #include <iostream>
 using namespace std;
 
-template <typename A, typename B = char> class Example {
+template <typename A, typename B = bool> class Example {
 public:
     A x;
     B y;
-    Example() { cout << "the constructor is activated" << endl; }
+    Example() { 
+        cout << "the constructor is activated with " 
+        << typeid(x).name() << " and " << typeid(y).name() << endl; }
 };
 
 int main()
 {
-    Example<int> example1; // Example<int, char>
+    Example<int> example1; // Example<int, bool>
     return 0;
 }
