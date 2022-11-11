a85d3fe1bffe9b13aff4ec6d9987d5c7d57c5908
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-11 23:24:34 +0200

    Update virtual_f01a.cpp

--- source_code/virtual_f01a.cpp
+++ source_code/virtual_f01a.cpp
@@ -18,15 +18,29 @@ public:
     void hello() { cout << "hello() from subclass\n"; }
 
     void yolo() { // extending hyperclass yolo
-        cout << "yolo() from subclass and ...\n"; 
-        hyperclass::yolo();
+        cout << "yolo() from subclass and ...\n";
+        //hyperclass::yolo();
     }
 };
 
+
+
+
+class subclass2 : public subclass {
+public:
+    void hello() { cout << "hello() from subclass2\n"; }
+
+    void yolo() { 
+        cout << "yolo() from subclass2 and ...\n";
+    }
+};
+
+
+
 int main() {
     hyperclass* hpr;
-    subclass sb;
-    hpr = &sb;
+    subclass2 sb2;
+    hpr = &sb2;
 
     // Virtual function
     // -> binded at runtime
